9772f43f93cca13a6bf664c62f8a7a58c3a6f7b4
Ondřej Seidl <xseidl06@vutbr.cz>
2023-04-02 13:42:05 +0200

    Refactor: added SqLiteFactory to sepparate file, started work on tests.

---
+++ Trackerino.DAL/Factories/SqLiteFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Trackerino.DAL.Factories
+{
+    public class SqLiteFactory : IDbContextFactory<TrackerinoDbContext>
+    {
+        public TrackerinoDbContext CreateDbContext()
+        {
+            DbContextOptionsBuilder<TrackerinoDbContext> builder = new();
+            builder.UseSqlite("Data Source=Trackerino.db");
+            return new TrackerinoDbContext(builder.Options);
+        }
+    }
+}
