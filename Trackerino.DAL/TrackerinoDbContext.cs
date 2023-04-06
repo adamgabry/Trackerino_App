d7950f01eb87bc3606644b0cb175e6f4d89d7ddd
Ondřej Seidl <xseidl06@vutbr.cz>
2023-04-06 20:30:09 +0200

    Fix
    This reverts commit b75980cbe314247e74ac648f279efbfd1936a647.

---
+++ Trackerino.DAL/TrackerinoDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Trackerino.DAL.Entities;
+using Trackerino.DAL.Factories;
+namespace Trackerino.DAL
+{
+    public class TrackerinoDbContext : DbContext
+    {
+        private readonly bool _seedDemoData;
+        public TrackerinoDbContext(DbContextOptions contextOptions, bool seedDemoData = false) : base(contextOptions) =>
+            _seedDemoData = seedDemoData;
+
+        public DbSet<UserProjectEntity> UserProject=> Set<UserProjectEntity>();
+        public DbSet<UserEntity> Users => Set<UserEntity>();
+        public DbSet<ProjectEntity> Projects => Set<ProjectEntity>();
+        public DbSet<ActivityEntity> Activities => Set<ActivityEntity>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserEntity>()
+                .HasMany(i => i.Projects)
+                .WithOne(i => i.User)
+                .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<ProjectEntity>()
+                .HasMany(i => i.Users)
+                .WithOne(i => i.Project)
+                .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<UserEntity>()
+                .HasMany(i => i.Activities)
+                .WithOne(i => i.User);
+            modelBuilder.Entity<ProjectEntity>()
+                .HasMany(i => i.Activities)
+                .WithOne(i => i.Project);
+        }
+
+    }
+}
