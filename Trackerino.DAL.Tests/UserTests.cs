0726e0e4a02bc064e2d8dda79eb7f1768ba82dfc
Ondřej Seidl <xseidl06@vutbr.cz>
2023-04-06 20:27:10 +0200

    Revert "Updated README.md"
    This reverts commit b75980cbe314247e74ac648f279efbfd1936a647.

--- Trackerino.DAL.Tests/UserTests.cs
+++ Trackerino.DAL.Tests/UserTests.cs
@@ -10,18 +10,20 @@ namespace Trackerino.DAL.Tests
         {
             _dbContextSUT = new Trackerino.DAL.Factories.DefaultFactory().CreateDbContext(new[] { "" });
         }
-
+        public Guid UserGuid = Guid.NewGuid();
         [Fact]
         public void NewUser_Add_Added()
         {
             var user = new UserEntity
             {
+                Id = UserGuid,
                 Name = "Pepa",
                 Surname = "Novak",
                 Projects = null,
             };
             _dbContextSUT.Users.Add(user);
             _dbContextSUT.SaveChanges();
+            Assert.Equal(_dbContextSUT.Users.Find(UserGuid),user);
         }
     }
 }
