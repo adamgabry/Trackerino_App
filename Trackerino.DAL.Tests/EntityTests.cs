d22cc00c5eadf2292f04d640073c934cda467c90
Matyáš Strelec <xstrel03@vutbr.cz>
2023-04-09 21:52:13 +0200

    begin work on entity tests

---
+++ Trackerino.DAL.Tests/EntityTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Trackerino.DAL.Entities;
+using Trackerino.DAL.Mappers;
+using Trackerino.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Trackerino.DAL.Factories;
+using Xunit;
+using System.Threading.Channels;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace Trackerino.DAL.Tests
+{
+    public class EntityTests : DbContextTestsBase
+    {
+        [Fact]
+        public async Task User_Add()
+        {
+            // Arrange
+            var user = new UserEntity
+            {
+                Id = Guid.Parse(input: "fabde0cd-eefe-443f-baf6-3d96cc2cbf2e"),
+                Name = "Name",
+                Surname = "Surname",
+                Projects = null
+            };
+
+            // Assert
+            Assert.Empty(await TrackerinoDbContextSUT.Users.ToListAsync());
+
+            // Act
+            await TrackerinoDbContextSUT.Users.AddAsync(user);
+            await TrackerinoDbContextSUT.SaveChangesAsync();
+            var result = await TrackerinoDbContextSUT.Users.ToListAsync();
+
+            // Assert
+            Assert.Single(result);
+            Assert.Equal(user, result[0]);
+        }
+
+        [Fact]
+        public async Task User_Delete()
+        {
+            // Arrange
+            var user = new UserEntity
+            {
+                Id = Guid.Parse(input: "fabde0cd-eefe-443f-baf6-3d96cc2cbf2e"),
+                Name = "Name",
+                Surname = "Surname",
+                Projects = null
+            };
+
+            // Act
+            await TrackerinoDbContextSUT.Users.AddAsync(user);
+            await TrackerinoDbContextSUT.SaveChangesAsync();
+            var result = await TrackerinoDbContextSUT.Users.ToListAsync();
+
+            // Assert
+            Assert.Single(result);
+            Assert.Equal(user, result[0]);
+
+            // Remove
+            TrackerinoDbContextSUT.Users.Remove(user);
+            await TrackerinoDbContextSUT.SaveChangesAsync();
+            var deleteResult = await TrackerinoDbContextSUT.Users.ToListAsync();
+
+            Assert.Empty(deleteResult);
+        }
+
+    }
+}
