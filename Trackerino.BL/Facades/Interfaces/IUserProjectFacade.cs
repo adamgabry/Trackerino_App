01b48f0d21ce1a00f646ac3d6a59698bc455874b
Matyáš Strelec <xstrel03@vutbr.cz>
2023-05-03 18:58:06 +0200

    add Facade Interfaces, update namespace

---
+++ Trackerino.BL/Facades/Interfaces/IUserProjectFacade.cs
@@ -0,0 +1,11 @@
+﻿using Trackerino.BL.Models;
+using Trackerino.DAL.Entities;
+
+namespace Trackerino.BL.Facades.Interfaces;
+
+public interface IUserProjectFacade : IFacade<UserProjectEntity, UserProjectListModel, UserProjectDetailModel>
+{
+    Task SaveAsync(UserProjectDetailModel model, Guid userId);
+    Task SaveAsync(UserProjectListModel model, Guid userId);
+    Task DeleteAsync(Guid id);
+}
