9bde8a3f2cbfa6b720f516458519034d8905da3d
Ondřej Seidl <xseidl06@vutbr.cz>
2023-05-04 00:46:37 +0200

    refactor: removed unused dependencies, refactored DbContext creation

---
+++ Trackerino.DAL/Factories/DesignTimeDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+namespace Trackerino.DAL.Factories;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<TrackerinoDbContext>
+{
+    private readonly DbContextLocalDbFactory _dbContextLocalDbFactory;
+    public DesignTimeDbContextFactory()
+    {
+        _dbContextLocalDbFactory = new DbContextLocalDbFactory("Trackerino", seedDemoData:false);
+    }
+
+    public TrackerinoDbContext CreateDbContext(string[] args) => _dbContextLocalDbFactory.CreateDbContext();
+}
+
