6c98a750b558c10b378160b27457469b2d17e7d2
Matyáš Strelec <xstrel03@vutbr.cz>
2023-05-08 11:37:37 +0200

    add installers & migrator

---
+++ Trackerino.App/AppInstaller.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using Trackerino.App.Services.Interfaces;
+using Trackerino.App.Services;
+using Trackerino.App.Shells;
+using Trackerino.App.ViewModels;
+using Trackerino.App.Views;
+
+namespace Trackerino.App;
+
+public static class AppInstaller
+{
+    public static IServiceCollection AddAppServices(this IServiceCollection services)
+    {
+        services.AddSingleton<AppShell>();
+
+        services.AddSingleton<IMessenger>(_ => StrongReferenceMessenger.Default);
+        services.AddSingleton<IMessengerService, MessengerService>();
+
+        services.AddSingleton<IAlertService, AlertService>();
+
+        services.Scan(selector => selector
+            .FromAssemblyOf<App>()
+            .AddClasses(filter => filter.AssignableTo<ContentPageBase>())
+            .AsSelf()
+            .WithTransientLifetime());
+
+        services.Scan(selector => selector
+            .FromAssemblyOf<App>()
+            .AddClasses(filter => filter.AssignableTo<IViewModel>())
+            .AsSelfWithInterfaces()
+            .WithTransientLifetime());
+
+        services.AddTransient<INavigationService, NavigationService>();
+
+        return services;
+    }
+}
---
+++ Trackerino.App/DbMigrator.cs
@@ -0,0 +1,17 @@
+﻿using Trackerino.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Trackerino.App;
+
+interface IDbMigrator
+{
+    public void Migrate();
+    public Task MigrateAsync(CancellationToken cancellationToken);
+}
+
+public class NoneDbMigrator : IDbMigrator
+{
+    public void Migrate() { }
+    public Task MigrateAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
