54c1c2f5f8d5eb00a027d8cfb6de62a9dc6b6167
Matyáš Strelec <xstrel03@vutbr.cz>
2023-05-25 13:14:55 +0200

    add converter for DateTime

---
+++ Trackerino.App/Converters/DateTimeToStringConverter.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Maui.Converters;
+using System.Globalization;
+
+namespace Trackerino.App.Converters
+{
+    public class DateTimeToStringConverter : BaseConverterOneWay<DateTime, string>
+    {
+        public override string ConvertFrom(DateTime value, CultureInfo? culture)
+        {
+            return value.ToString("yyyy-MM-dd, HH:mm", culture);
+        }
+
+        public override string DefaultConvertReturnValue { get; set; } = string.Empty;
+    }
+}
