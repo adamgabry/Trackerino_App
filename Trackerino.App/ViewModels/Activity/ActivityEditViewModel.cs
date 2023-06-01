d3e105f9bf90d513b3bf8ebe6873d9be8d3c25d6
Ondřej Seidl <xseidl06@vutbr.cz>
2023-06-01 18:49:13 +0200

    Checking for overlaping activities

--- Trackerino.App/ViewModels/Activity/ActivityEditViewModel.cs
+++ Trackerino.App/ViewModels/Activity/ActivityEditViewModel.cs
@@ -5,6 +5,7 @@ using Trackerino.App.Services;
 using Trackerino.BL.Facades.Interfaces;
 using Trackerino.BL.Models;
 using System;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Trackerino.App.ViewModels
@@ -14,6 +15,8 @@ namespace Trackerino.App.ViewModels
     {
         private readonly IActivityFacade _activityFacade;
         private readonly INavigationService _navigationService;
+
+        public IEnumerable<ActivityListModel> Activities { get; set; } = null!;
 
         public DateTime StartDate { get; set; }
         public TimeSpan StartTime { get; set; }
@@ -43,16 +46,36 @@ namespace Trackerino.App.ViewModels
         [RelayCommand]
         private async Task SaveAsync()
         {
+            //Get values from pickers
             DateTime start = StartDate.Date;
             DateTime end = EndDate.Date;
             Activity.StartDateTime = start.AddMinutes(StartTime.TotalMinutes);
             Activity.EndDateTime = end.AddMinutes(EndTime.TotalMinutes);
-            await _activityFacade.SaveAsync(Activity);
-            
-            MessengerService.Send(new ActivityEditMessage { ActivityId = Activity.Id });
-            _navigationService.SendBackButtonPressed();
+            //check for overlaps
+            IEnumerable<ActivityListModel> existingActivities = await _activityFacade.GetAsync();
+            if (!CheckActivityOverlap(existingActivities))
+            {
+                await _activityFacade.SaveAsync(Activity);
+                MessengerService.Send(new ActivityEditMessage { ActivityId = Activity.Id });
+                _navigationService.SendBackButtonPressed();
+            }
         }
-
+        private bool CheckActivityOverlap(IEnumerable<ActivityListModel> existingActivities)
+        {
+            foreach (var i in existingActivities)
+            {
+                if (i.Id != Activity.Id)
+                {
+                    if (Activity.StartDateTime>= i.StartDateTime && Activity.StartDateTime < i.EndDateTime)
+                        return true;
+                    if (Activity.EndDateTime > i.StartDateTime && Activity.EndDateTime <= i.EndDateTime)
+                        return true;
+                    if(Activity.StartDateTime <= i.StartDateTime && Activity.EndDateTime >= i.EndDateTime)
+                        return true;
+                }
+            }
+            return false;
+        }
         private Task UpdatePickers()
         {
             StartDate = Activity.StartDateTime.Date;
