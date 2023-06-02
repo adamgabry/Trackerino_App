e3ea1373564e75438c7293ea6de6b2cdbf8db798
Matyáš Strelec <xstrel03@vutbr.cz>
2023-06-02 10:17:02 +0200

    fix not logged in user

--- Trackerino.App/Shells/AppShell.xaml.cs
+++ Trackerino.App/Shells/AppShell.xaml.cs
@@ -7,27 +7,59 @@ namespace Trackerino.App.Shells;
 public partial class AppShell
 {
     private readonly INavigationService _navigationService;
+    private readonly IAlertService _alertService;
 
-    public AppShell(INavigationService navigationService)
+    public AppShell(INavigationService navigationService, IAlertService alertService)
     {
         _navigationService = navigationService;
-
+        _alertService = alertService;
         InitializeComponent();
     }
 
     [RelayCommand]
     private async Task GoToLoginAsync()
-        => await _navigationService.GoToAsync<UserLoginViewModel>();
+    {
+        Preferences.Set("ActiveUser", Guid.Empty.ToString());
+        await _navigationService.GoToAsync<UserLoginViewModel>();
+    }
 
     [RelayCommand]
     private async Task GoToUsersAsync()
-        => await _navigationService.GoToAsync<UserListViewModel>();
+    {
+        if (Guid.Parse(Preferences.Get("ActiveUser", string.Empty)) == Guid.Empty)
+        {
+            await _alertService.DisplayAsync("Failed to login", "You must first login");
+        }
+        else
+        {
+            await _navigationService.GoToAsync<UserListViewModel>();
+        }
+    }
 
     [RelayCommand]
     private async Task GoToProjectsAsync()
-        => await _navigationService.GoToAsync<ProjectListViewModel>();
+    {
+        if (Guid.Parse(Preferences.Get("ActiveUser", string.Empty)) == Guid.Empty)
+        {
+            await _alertService.DisplayAsync("Failed to login", "You must first login");
+        }
+        else
+        {
+            await _navigationService.GoToAsync<ProjectListViewModel>();
+        }
+    }
 
     [RelayCommand]
     private async Task GoToActivitiesAsync()
-        => await _navigationService.GoToAsync<ActivityListViewModel>();
+    {
+        if (Guid.Parse(Preferences.Get("ActiveUser", string.Empty)) == Guid.Empty)
+        {
+            await _alertService.DisplayAsync("Failed to login", "You must first login");
+        }
+        else
+        {
+            await _navigationService.GoToAsync<ActivityListViewModel>();
+        }
+
+    }
 }
